02c78b916bb9f721432b6bea44a0758dfb2c9b3c
weima-sage <wei.ma@sage.com>
2015-06-15 12:22:37 -0700

    code refatoring

--- Chapter16/BindingSourceCode/BindingSourceCode/BindingSourceCode/BindingSourceCodePage.cs
+++ Chapter16/BindingSourceCode/BindingSourceCode/BindingSourceCode/BindingSourceCodePage.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using Xamarin.Forms;
+using System.Linq.Expressions;
 
 namespace BindingSourceCode
 {
     public class BindingSourceCodePage : ContentPage
     {
+        private Binding GetBinding<T> (T source, Expression< Func<T,object>> func) where T:BindableObject
+        {
+            Binding binding = Binding.Create<T>(func);
+            binding.Source = source;
+            return binding;
+        }
+
         public BindingSourceCodePage()
         {
+
             Label label = new Label
             {
                 Text = "Binding Source Demo",
@@ -21,11 +30,7 @@ namespace BindingSourceCode
             };
 
             // Define Binding object with source object and property.
-            Binding binding = new Binding
-            {
-                Source = slider,
-                Path = nameof(Slider.Value)
-            };
+            Binding binding = GetBinding(slider, src => src.Value);
 
             // Bind the Opacity property of the Label to the source.
             label.SetBinding(Label.OpacityProperty, binding);
